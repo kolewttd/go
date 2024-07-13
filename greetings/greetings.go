4524672a09c4197f506357eb75e5df7dac311c9b
kolewttd <kolewtt@proton.me>
2024-07-13 12:44:21 +0200

    Fix a missing quote in greetings.go.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -2,7 +2,7 @@ package greetings
 
 import "fmt"
 
-func Hello(name string) string{
-	message := fmt.Sprintf("Hi, %v. Welcome!, name)
+func Hello(name string) string {
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
 }
---
+++ pkg/mod/github.com/kolewttd/go/greetings@v0.0.0-20240713103610-6274f2b7e876/go.mod
@@ -0,0 +1,3 @@
+module github.com/kolewttd/go/greetings
+
+go 1.22.2
